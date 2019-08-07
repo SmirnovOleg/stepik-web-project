8d1ece136dc43660ac652a48f881da8fd4377132
Oleg Smirnov <sm-oleg1209@yandex.ru>
2019-08-08 00:40:27 +0700

    Question and Answer models added

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=300)
+    added_at = models.DateTimeField()
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=300)
+    added_at = models.DateTimeField()
+    question = models.OneToOneField(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.text
