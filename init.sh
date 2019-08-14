5762517ef5fc74f157158ac333cd6324bf4f9856
Oleg Smirnov <sm-oleg1209@yandex.ru>
2019-08-14 21:56:56 +0700

    .gitignore and deploying fixes

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo rm -rf /etc/nginx/sites-*/*
 sudo ln -s /home/box/web/etc/app-nginx.conf  /etc/nginx/sites-enabled/app-nginx.conf
 sudo service nginx restart
-cd /home/box/web/ask
-sudo gunicorn --config=../etc/gunicorn.conf ask.wsgi
+sudo gunicorn --conf='etc/gunicorn.conf' ask.wsgi
